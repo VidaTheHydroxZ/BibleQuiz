9bb5b5310134698cf8237a2cf9575b01e389b6f9
VidaTheHydroxZ <david.vidakovic01@gmail.com>
2024-10-02 23:19:20 +0200

    Added second activity and some images

--- app/src/main/java/com/example/biblequiz/MainActivity.java
+++ app/src/main/java/com/example/biblequiz/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.biblequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v)
+    {
+        Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+        startActivity(intent);
     }
 }
